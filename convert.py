9af9c603844b2a990789b2dff99c6d0c10c968f1
dawnineyes <42796622+dawnineyes@users.noreply.github.com>
2025-11-23 04:55:37 -0800

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,28 @@
+import subprocess
+import requests
+import os
+
+def convert3():
+    # 1. 下载 adguard dns filer
+    url = "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt"
+    local_file = "sing-box/adguard_dns_filter"
+
+    r = requests.get(url)
+    with open(local_file, "w", encoding="utf-8") as f:
+        f.write(r.text)
+
+    # 2. 使用 Linux 的 sing-box 编译
+    cmd = [
+        "./sing-box", "rule-set", "convert",
+        "--type", "adguard", "sing-box/adguard_dns_filter"
+    ]
+
+    try:
+        result = subprocess.run(cmd, check=True, capture_output=True, text=True)
+        print("编译成功！")
+    except subprocess.CalledProcessError as e:
+        print("编译失败：")
+        print(e.stderr)
+
+if __name__ == '__main__':
+    convert3()
